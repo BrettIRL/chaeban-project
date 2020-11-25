92748707190ce0c002926c53584a5bb9bc5208ba
Brett <brett@pop-os.localdomain>
2020-11-25 16:19:04 +0200

    :sparkles: Implement react router

--- src/scenes/App/index.tsx
+++ src/scenes/App/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import InputForm from "components/InputForm";
+import OutputPage from "components/OutputPage";
 
 function App() {
   return (
-    <main className="container max-w-screen-lg">
-      <InputForm />
-    </main>
+    <Router>
+      <Switch>
+        <Route path="/output" component={OutputPage} />
+      </Switch>
+    </Router>
   );
 }
 
