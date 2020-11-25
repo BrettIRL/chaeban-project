dfaabecf74cb9d8df80ab0f82271ec71220d808e
Brett <brett@pop-os.localdomain>
2020-11-25 16:17:58 +0200

    :heavy_plus_sign: Add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "12xl": "12rem",
+      },
+      colors: {
+        primary: "#2f363d",
+        secondary: "#3e4a4c",
+        tertiary: "#525a5c",
+        accent: "#78ce6d",
+        "accent-light": "#8cd582",
+      },
+      width: {
+        "3/10": "30%",
+        "6/10": "60%",
+        "9/10": "90%",
+      },
+    },
     container: {
       center: true,
     },
